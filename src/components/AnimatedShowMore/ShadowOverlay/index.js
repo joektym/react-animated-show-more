909d0c97e452800899e021666ade051255795377
Joe Kalayil <joejkalayil@yahoo.com>
2022-06-09 20:29:54 -0400

    Update shadow gradient code
    Use rgba instead of transparent to fix iOS Safari grey overlay issue

--- src/components/AnimatedShowMore/ShadowOverlay/index.js
+++ src/components/AnimatedShowMore/ShadowOverlay/index.js
@@ -5,7 +5,7 @@ export const ShadowOverlay = ({ color = '#fff' }) => (
   <div
     className="AnimatedShowMore__ShadowOverlay"
     style={{
-      backgroundImage: `linear-gradient(to bottom, transparent, ${color})`,
+      backgroundImage: `linear-gradient(to bottom, rgba(255, 255, 255, 0), ${color})`,
       position: 'absolute',
       bottom: 0,
       left: 0,
